cb93c794a8ebee0989bf1c8dfe58b878ffac656a
kvvzr <kwzr@twinkrun.net>
2015-01-07 15:37:10 +0900

    prepare for database

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, json, jsonify
-from flaskext.mysql import MySQL
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 from datetime import datetime as dt
 import mido
 import orpheus.lyrics as Lyrics
@@ -11,10 +13,7 @@ import orpheus.melody as Melody
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
+# router
 @app.route('/analyze_lyrics')
 def analyze_lyrics():
     return jsonify({'lyrics': Lyrics.analyze(request.args.get('text', default=''))})
@@ -60,6 +59,127 @@ def compose():
         return 'KeyError'
     return 'Hello'
 
+# db setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://orpheus:kumapanda@localhost/orpheus'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    screen_name = db.Column(db.String(255), nullable=False)
+    passwd = db.Column(db.String(255), nullable=False)
+    icon = db.Column(db.LargeBinary, nullable=True, default=None)
+
+class Rhythms(db.Model):
+    __tablename__ = 'rhythms'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    data = db.Column(db.Text, nullable=False)
+
+class Chords(db.Model):
+    __tablename__ = 'chords'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    data = db.Column(db.Text, nullable=False)
+
+class Accoms(db.Model):
+    __tablename__ = 'accoms'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    data = db.Column(db.Text, nullable=False)
+
+class Drums(db.Model):
+    __tablename__ = 'drums'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    data = db.Column(db.Text, nullable=False)
+
+class Musics(db.Model):
+    __tablename__ = 'musics'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    raw_midi = db.Column(db.LargeBinary, nullable=True, default=None)
+    fork_count = db.Column(db.Integer, nullable=False, default=0)
+    fork_from = db.Column(db.Integer, nullable=True, default=None)
+    star_count = db.Column(db.Integer, nullable=False, default=0)
+    play_count = db.Column(db.Integer, nullable=False, default=0)
+    data = db.Column(db.Text, nullable=False)
+
+class StaredUsers(db.Model):
+    __tablename__ = 'stared_users'
+    id = db.Column(db.Integer, primary_key=True)
+    stared_user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+class StaredRhythms(db.Model):
+    __tablename__ = 'stared_rhythms'
+    id = db.Column(db.Integer, primary_key=True)
+    rhythms_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+class StaredChords(db.Model):
+    __tablename__ = 'stared_chords'
+    id = db.Column(db.Integer, primary_key=True)
+    chords_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+class StaredAccoms(db.Model):
+    __tablename__ = 'stared_accoms'
+    id = db.Column(db.Integer, primary_key=True)
+    accoms_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+class StaredDrums(db.Model):
+    __tablename__ = 'stared_drums'
+    id = db.Column(db.Integer, primary_key=True)
+    drums_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+class StaredMusics(db.Model):
+    __tablename__ = 'stared_musics'
+    id = db.Column(db.Integer, primary_key=True)
+    music_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+class RhythmComments(db.Model):
+    __tablename__ = 'rhythm_comments'
+    id = db.Column(db.Integer, primary_key=True)
+    rhythms_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(1023), nullable=False)
+
+class ChordComments(db.Model):
+    __tablename__ = 'chord_comments'
+    id = db.Column(db.Integer, primary_key=True)
+    chords_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(1023), nullable=False)
+
+class AccomComments(db.Model):
+    __tablename__ = 'accom_comments'
+    id = db.Column(db.Integer, primary_key=True)
+    accoms_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(1023), nullable=False)
+
+class DrumComments(db.Model):
+    __tablename__ = 'drum_comments'
+    id = db.Column(db.Integer, primary_key=True)
+    drums_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(1023), nullable=False)
+
+class MusicComments(db.Model):
+    __tablename__ = 'music_comments'
+    id = db.Column(db.Integer, primary_key=True)
+    music_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(1023), nullable=False)
+
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    manager.run()
