4dff15f10a3652f554a085b54e5993ab34b034a8
kvvzr <kwzr@twinkrun.net>
2015-08-24 06:32:30 +0900

    Enable to try sample lyrics easily #2

--- static/js/composer.js
+++ static/js/composer.js
@@ -6,7 +6,7 @@
 
         $('#btn-sample').on('click', function(event) {
             $('#music-title').val('サンプル音楽');
-            $('#music-lyrics').val('ここに漢字仮名交じりの歌詞を入力して\n===\nこうやって歌詞を区切ることができます\n===\nスペースで 小節を 細かく 分けられます');
+            $('#music-lyrics').val('ここに日本語の歌詞を入力して\n===\nこうやって歌詞を区切ることができます\n===\nスペースで 小節を 細かく 分けられます');
         });
 
         $('#btn-analyze').on('click', function(event) {
