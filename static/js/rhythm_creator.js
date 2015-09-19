a13f499d816b174734e62f3d371550f77aceb5a7
kvvzr <kwzr@twinkrun.net>
2015-09-19 09:31:08 +0900

    rhythm creators

--- static/js/rhythm_creator.js
+++ static/js/rhythm_creator.js
@@ -1,15 +1,49 @@
 ;(function(window, $) {
     'use strict';
 
+    var moras = [8, 16, 32];
+    var times = [1, 2];
+
+    var mora = 8;
+    var time = 1;
+
     $(document).ready(function() {
-        initCreator($('.rc-container'), 8);
+        initCreator(mora, time);
+    });
+
+    var initCreator = function(mora, time) {
+        var $container = $('#rc-container');
+
+        $container.empty();
+
+        $container.append("<select id='mora-selector' class='selector'></select>");
+        var $mora_selector = $('#mora-selector');
+        for (var i = 0; i < moras.length; i++) {
+            $mora_selector.append('<option data-content="<span data-mora=&quot;' + moras[i] + '&quot;>' + moras[i] + 'モーラ</span>">' + moras[i] + 'モーラ</option>');
+        }
+        $mora_selector.val(mora + 'モーラ');
+
+        $container.append("<select id='time-selector' class='selector'></select>");
+        var $time_selector = $('#time-selector');
+        for (var i = 0; i < times.length; i++) {
+            $time_selector.append('<option data-content="<span data-time=&quot;' + times[i] + '&quot;>' + times[i] + '小節</span>">' + times[i] + '小節</option>');
+        }
+        $time_selector.val(time + '小節');
+
         $('.selector').selectpicker({
             tickIcon: '',
         });
-    });
 
-    var initCreator = function($container, count) {
-        for (var i = 0; i < count - 1; i++) {
+        $mora_selector.on('change', function(){
+            mora = $('[data-id=mora-selector] > span > span').data('mora');
+            initCreator(mora, time);
+        });
+        $time_selector.on('change', function(){
+            time = $('[data-id=time-selector] > span > span').data('time');
+            initCreator(mora, time);
+        });
+
+        for (var i = 0; i < mora - 1; i++) {
             $container.append('<p>' + (i + 1) + 'モーラ</p>');
             $container.append("<div class='rhythm-creator'></div>");
         }
@@ -22,26 +56,26 @@
                 p.setup = function() {
                     p.createCanvas($node.width(), $node.height());
                     p.noStroke();
-                    for (var i = 0; i < count; i++) {
+                    for (var i = 0; i < mora * time; i++) {
                         p.rhythms[i] = false;
                     }
                 };
 
                 p.draw = function() {
                     p.background(192);
-                    for (var i = 0; i < count; i++) {
+                    for (var i = 0; i < mora * time; i++) {
                         if (p.rhythms[i]) {
                             p.fill(219, 228, 228);
                         } else {
                             p.fill(255);
                         }
-                        p.rect(i * (p.width - 1) / count + 1, 1, (p.width - 1) / count - 1, p.height - 2);
+                        p.rect(i * (p.width - 1) / (mora * time) + 1, 1, (p.width - 1) / (mora * time) - 1, p.height - 2);
                     }
                 };
 
                 p.mousePressed = function() {
-                    for (var i = 0; i < count; i++) {
-                        if (i * p.width / count < p.mouseX && p.mouseX < (i + 1) * p.width / count && 1 < p.mouseY && p.mouseY < p.height) {
+                    for (var i = 0; i < mora * time; i++) {
+                        if (i * p.width / (mora * time) < p.mouseX && p.mouseX < (i + 1) * p.width / (mora * time) && 1 < p.mouseY && p.mouseY < p.height) {
                             p.rhythms[i] = !p.rhythms[i];
                         }
                     }
